ed36dc78dbc94f500c2e8ef7cd120c58c619d296
KauaSdias <153536047+KauaSdias@users.noreply.github.com>
2024-05-02 16:44:08 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 let mensagem = document.getElementById('mensagem')
-let espaco = document.getElementById("campoDeMensagem")
+let espaco = document.getElementById('campoDeMensagem')
+let imgbtn = document.getElementById('img')
+let menuobj = document.querySelector('.menu')
+let soltarimg = document.querySelector('.ContainerImagem')
+
+let falasPato = [
+    'Opa', 'Sim', 'Não', 'Talvez', 'Isso é meio paia', 'Que papinho em?', 'Tá', 'Isso ai', 'Acho que você está certo', 'blz', 'dboa', 'É'
+]
 
 window.addEventListener("keydown", function (e){
     if (e.key === "Enter") {
@@ -11,12 +18,75 @@ window.addEventListener("keydown", function (e){
 });
 
 function enviar() {
-    let novamsg = document.createElement('div')
-    novamsg.setAttribute('class', 'mensagem')
-    let texto = document.createElement('p')
-    texto.innerText = mensagem.value
-    novamsg.appendChild(texto)
+    if (mensagem.value.trim() !== "") {
+            let novamsg = document.createElement('div');
+            novamsg.setAttribute('class', 'mensagem');
+            let texto = document.createElement('p');
+            texto.innerText = mensagem.value;
+            novamsg.appendChild(texto);
+    
+            espaco.appendChild(novamsg);
+            mensagem.value = '';
+            novamsg.scrollIntoView()
 
-    espaco.appendChild(novamsg)
-    mensagem.value = ""
+            pato()
+    }
+}
+
+
+function menu(estado){
+    if (estado) {
+        menuobj.style.display = 'flex'
+    } else {
+        menuobj.style.display = 'none'
+    }
+}
+
+
+function enviarFoto(estado) {
+    if (estado == 0){
+        soltarimg.style.display = 'flex'
+    } else {
+        soltarimg.style.display = 'none'
+        // pega o elemento input de arquivo
+        const inputImagem = document.getElementById('image').querySelector('input[type="file"]');
+        
+        // Verifica se inputImagem.files contém arquivos
+        if (inputImagem.files.length > 0) {
+            const arquivo = inputImagem.files[0];
+            // Cria um FileReader
+            const reader = new FileReader();
+            // Define o que acontecerá quando o arquivo for lido
+            reader.onload = function(e) {
+                // Cria um elemento de imagem e define seu src
+                let novaImagem = document.createElement('img');
+                novaImagem.src = e.target.result;
+
+                // Adiciona a nova imagem ao campo de mensagem
+                espaco.appendChild(novaImagem);
+            };
+
+
+
+            // Lê o arquivo como um URL de dados
+            reader.readAsDataURL(arquivo);
+        } else {
+            alert("Nenhum arquivo selecionado.");
+        }       
+    }
+}
+
+
+function pato(){
+    let number = Math.floor(Math.random() * 12)
+    let fala = falasPato[number]
+
+    let novamsg = document.createElement('div');
+        novamsg.setAttribute('class', 'mensagemPato');
+        let texto = document.createElement('p');
+        texto.innerText = fala;
+        novamsg.appendChild(texto);
+
+        espaco.appendChild(novamsg);
+        novamsg.scrollIntoView()
 }
