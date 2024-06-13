53cb43f3bd7563c4ec0064991e8a15a5661f26a5
KauaSdias <153536047+KauaSdias@users.noreply.github.com>
2024-06-13 15:47:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,36 @@ let imgbtn = document.getElementById('img')
 let menuobj = document.querySelector('.menu')
 let soltarimg = document.querySelector('.ContainerImagem')
 
+
+let mic = document.querySelector('.mic')
+let mutado = true
+ mic.onclick = function () {
+    if (mutado){
+        mic.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="m710-362-58-58q14-23 21-48t7-52h80q0 44-13 83.5T710-362ZM480-594Zm112 112-72-72v-206q0-17-11.5-28.5T480-800q-17 0-28.5 11.5T440-760v126l-80-80v-46q0-50 35-85t85-35q50 0 85 35t35 85v240q0 11-2.5 20t-5.5 18ZM440-120v-123q-104-14-172-93t-68-184h80q0 83 57.5 141.5T480-320q34 0 64.5-10.5T600-360l57 57q-29 23-63.5 39T520-243v123h-80Zm352 64L56-792l56-56 736 736-56 56Z"/></svg>'
+        mutado = false
+    } else {
+        mic.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M480-400q-50 0-85-35t-35-85v-240q0-50 35-85t85-35q50 0 85 35t35 85v240q0 50-35 85t-85 35Zm0-240Zm-40 520v-123q-104-14-172-93t-68-184h80q0 83 58.5 141.5T480-320q83 0 141.5-58.5T680-520h80q0 105-68 184t-172 93v123h-80Zm40-360q17 0 28.5-11.5T520-520v-240q0-17-11.5-28.5T480-800q-17 0-28.5 11.5T440-760v240q0 17 11.5 28.5T480-480Z"/></svg>'
+        mutado = true
+    }
+    
+ } 
+
+let modal = document.querySelector('dialog')
+let btCall = document.getElementById('call')
+let endCall = document.querySelector('.end')
+
+btCall.onclick = function () {
+    modal.style.display = 'flex'
+    conte()
+}
+
+endCall.onclick = function () {
+    modal.style.display = 'none'
+    pareConte()
+}
+
+
+
 let falasPato = [
     'Opa', 'Sim', 'Não', 'Talvez', 'Isso é meio paia', 'Que papinho em?', 'Tá', 'Isso ai', 'Acho que você está certo', 'blz', 'dboa', 'É'
 ]
@@ -76,7 +106,6 @@ function enviarFoto(estado) {
     }
 }
 
-
 function pato(){
     let number = Math.floor(Math.random() * 12)
     let fala = falasPato[number]
@@ -89,4 +118,48 @@ function pato(){
 
         espaco.appendChild(novamsg);
         novamsg.scrollIntoView()
+}
+
+let temp = {
+    m: 0,
+    s: 0,
+    ms: 0,
+}
+let horas;
+
+function conte() {
+  pause();
+  horas = setInterval(() => { timer(); }, 10);
+}
+
+function pause() {
+  clearInterval(horas);
+}
+
+function pareConte() {
+  temp.m = 0;
+  temp.s = 0;
+
+  document.getElementById('m').innerText = '00';
+  document.getElementById('s').innerText = '00';
+}
+
+function timer() {
+  if ((temp.ms += 10) == 1000) {
+    temp.ms = 0;
+    temp.s++;
+  }
+  if (temp.s == 60) {
+    temp.s = 0;
+    temp.m++;
+  }
+  if (temp.m == 60) {
+    temp.m = 0;
+  }
+  document.getElementById('m').innerText = returnData(temp.m);
+  document.getElementById('s').innerText = returnData(temp.s);
+}
+
+function returnData(input) {
+  return input >= 10 ? input : `0${input}`
 }
